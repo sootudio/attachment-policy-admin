3203bb80d7b91447dbbd8b114f021f7585798366
Sooyoung <kswim8914@gmail.com>
2026-01-31 16:38:40 +0900

    chore: 실행시 세부적인 에러 확인을 위한 코드 추가

--- src/main/java/com/sootudio/extguard/global/exception/GlobalExceptionHandler.java
+++ src/main/java/com/sootudio/extguard/global/exception/GlobalExceptionHandler.java
@@ -1,11 +1,13 @@
 package com.sootudio.extguard.global.exception;
 
 import com.sootudio.extguard.global.response.ApiResponse;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+@Slf4j
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
@@ -28,8 +30,16 @@ public class GlobalExceptionHandler {
                 .body(ApiResponse.error(org.springframework.http.HttpStatus.BAD_REQUEST, "VALIDATION_ERROR", msg));
     }
 
+    @ExceptionHandler(org.springframework.http.converter.HttpMessageNotReadableException.class)
+    public ResponseEntity<ApiResponse<Void>> handleNotReadable(Exception ex) {
+        return ResponseEntity
+                .badRequest()
+                .body(ApiResponse.error(org.springframework.http.HttpStatus.BAD_REQUEST, "INVALID_REQUEST", "invalid json body"));
+    }
+
     @ExceptionHandler(Exception.class)
     public ResponseEntity<ApiResponse<Void>> handleGeneral(Exception ex) {
+        log.error("Unhandled exception", ex);
         ErrorCode ec = ErrorCode.INTERNAL_ERROR;
         return ResponseEntity
                 .status(ec.status())
