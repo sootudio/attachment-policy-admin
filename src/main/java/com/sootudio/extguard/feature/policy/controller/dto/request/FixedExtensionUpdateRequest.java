9f75a5527488328a5a828354b422f9b403be59f4
Sooyoung <kswim8914@gmail.com>
2026-01-31 18:10:34 +0900

    chore: 요청 시 값 검증 메세지 수정

--- src/main/java/com/sootudio/extguard/feature/policy/controller/dto/request/FixedExtensionUpdateRequest.java
+++ src/main/java/com/sootudio/extguard/feature/policy/controller/dto/request/FixedExtensionUpdateRequest.java
@@ -3,5 +3,6 @@ package com.sootudio.extguard.feature.policy.controller.dto.request;
 import jakarta.validation.constraints.NotNull;
 
 public record FixedExtensionUpdateRequest(
-        @NotNull Boolean blocked
+        @NotNull(message = "blocked 값이 필요합니다.")
+        Boolean blocked
 ) {}
