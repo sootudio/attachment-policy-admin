9825f06446a9a424d74ca927aef11a12aebe8643
Sooyoung <kswim8914@gmail.com>
2026-01-31 17:07:02 +0900

    chore: 커스텀 확장자 에러코드 추가

--- src/main/java/com/sootudio/extguard/global/exception/ErrorCode.java
+++ src/main/java/com/sootudio/extguard/global/exception/ErrorCode.java
@@ -4,11 +4,15 @@ import org.springframework.http.HttpStatus;
 
 public enum ErrorCode {
     // 공통
-    INVALID_REQUEST(HttpStatus.BAD_REQUEST, "INVALID_REQUEST", "invalid request"),
-    INTERNAL_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "INTERNAL_ERROR", "internal server error"),
+    INVALID_REQUEST(HttpStatus.BAD_REQUEST, "INVALID_REQUEST", "잘못된 요청입니다."),
+    INTERNAL_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "INTERNAL_ERROR", "서버 에러입니다."),
 
     // 고정 확장자
-    FIXED_EXTENSION_NOT_FOUND(HttpStatus.NOT_FOUND, "FIXED_EXTENSION_NOT_FOUND", "fixed extension not found");
+    FIXED_EXTENSION_NOT_FOUND(HttpStatus.NOT_FOUND, "FIXED_EXTENSION_NOT_FOUND", "해당 고정 확장자가 없습니다."),
+
+    // 커스텀 확장자
+    CUSTOM_EXTENSION_DUPLICATED(HttpStatus.CONFLICT, "CUSTOM_EXTENSION_DUPLICATED", "해당 커스텀 확장자가 이미 있습니다."),
+    CUSTOM_EXTENSION_LIMIT_EXCEEDED(HttpStatus.BAD_REQUEST, "CUSTOM_EXTENSION_LIMIT_EXCEEDED", "커스텀 확장자는 최대 200개까지만 추가할 수 있습니다.");
 
     private final HttpStatus status;
     private final String code;
