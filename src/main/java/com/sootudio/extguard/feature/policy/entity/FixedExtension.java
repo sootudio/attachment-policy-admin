68124eb96cb65344559537866ad55216dead5ed6
Sooyoung <kswim8914@gmail.com>
2026-01-31 16:37:24 +0900

    feat: FixedExtension 엔티티에서 getter, NoArgsContstructor 사용. setBlocked를 좀더 범용적인 이름으로 변경

--- src/main/java/com/sootudio/extguard/feature/policy/entity/FixedExtension.java
+++ src/main/java/com/sootudio/extguard/feature/policy/entity/FixedExtension.java
@@ -1,8 +1,14 @@
 package com.sootudio.extguard.feature.policy.entity;
 
 import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
 import java.time.LocalDateTime;
 
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Entity
 @Table(name = "fixed_extension")
 public class FixedExtension {
@@ -17,18 +23,12 @@ public class FixedExtension {
     @Column(name = "updated_at", nullable = false, insertable = false, updatable = false)
     private LocalDateTime updatedAt;
 
-    protected FixedExtension() { }
-
     public FixedExtension(String extension, boolean blocked) {
         this.extension = extension;
         this.blocked = blocked;
     }
 
-    public String getExtension() { return extension; }
-    public boolean isBlocked() { return blocked; }
-    public LocalDateTime getUpdatedAt() { return updatedAt; }
-
-    public void setBlocked(boolean blocked) {
+    public void changeBlocked(boolean blocked) {
         this.blocked = blocked;
     }
 }
